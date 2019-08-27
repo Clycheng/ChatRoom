584c8c91c571a619797d62f287be340d819113f9
cylecheng <1249587790@qq.com>
2019-08-27 18:40:02 +0800

    8,.27

--- chatroom/src/components/common/Chat.vue
+++ chatroom/src/components/common/Chat.vue
@@ -1,25 +1,40 @@
 <template>
   <div>
       <header>{{chat}}</header>
-      <ul class = "list">
-          <li v-for = "(item,index) in list" :key = "index">
-              <img :src = "item.gimg" alt="">
-              <i>{{item.gname}}</i>
+      <ul v-if="chatCat == 1" class = "list group-list">
+          <li   v-for = "(item,index) in groupList" :key = "index" :class ="{active:index == isactive}"  @click="addactive(index)">
+              <div class ="head-pic-warp"><img :src = "item.Gheadimg" alt=""></div>
+              <div class = "head-name-warp"><i>{{item.groupName}}</i></div>
+          </li>
+      </ul>
+       <ul v-if="chatCat == 2"  class = "list per-list">
+          <li   v-for = "(item,index) in perList" :key = "index" :class ="{active:index == isactive}"  @click="addactive(index)">
+              <div class ="head-pic-warp"><img :src = "item.headPic" alt=""></div>
+              <div class = "head-name-warp"><i>{{item.intname}}</i></div>
           </li>
       </ul>
   </div>
 </template>
 
 <script>
-import { constants } from 'fs';
+import { constants } from 'crypto';
+import { type } from 'os';
 export default {
     name:"Chat",
     props:["chatCat"],
     data(){
         return{
-            list:[
+            isactive:0,
+            groupList:[
 
-            ]
+            ],
+            perList:[]
+        }
+    },
+    methods:{
+        addactive(res){
+            console.log(res)
+            this.isactive = res
         }
     },
  computed:{
@@ -33,8 +48,27 @@ export default {
  },
     mounted(){
         let groupCat =  sessionStorage.getItem('groupCat')
-        this.list = []
-        this.list.push(JSON.parse(groupCat))
+        this.axios.get('http://47.98.213.151:8090/qClass?id='+groupCat).then(res=>{
+            this.groupList = res.data
+        })
+        let perList = sessionStorage.getItem('firendGroup')
+        // console.log(perList)
+        perList = perList.split('},')
+        for (let index = 0; index < perList.length-1; index++) {
+            perList [index] += '}'
+        }
+   perList.forEach(element => {
+       this.perList.push(eval('(' + element + ')'))
+    // console.log(eval('(' + element + ')'))
+   });
+   console.log(this.perList)
+
+        // this.perList = sessionStorage.getItem('firendGroup').split("}")
+        //     this.perList.forEach(element => {
+        //         element = JSON.parse(JSON.stringify(element =element.replace(/\s*/g,"")))
+        //        console.log(element)
+        //     });
+        // this.list.push(JSON.parse(JSON.stringify(groupCat)))
     }
 
 }
@@ -45,18 +79,34 @@ export default {
         width: 100%;
         height: 100%;
     }
+    .list li.active{
+        background: #c8c6c6
+    }
     .list li {
         text-align: left;
         position: relative;
         width: 100%;
         height: 85px;
-        line-height: 85px;
         /* background: black; */
     }
-    .list li img {
-        display: block;
-        float: left;
-        width: 20%;
-        height: 70%;
-    }
+     .list li .head-pic-warp {
+         float: left;
+         width:25%;
+         height: 100%;
+     }
+     .list li .head-pic-warp  img {
+         margin-top: 10px;
+         margin-left: 20px;
+         width: 50%;
+     }
+     .list li .head-name-warp{
+         float: right;
+            width:75%;
+            line-height: 350%;
+         height: 100%;
+     }
+     .list li .head-name-warp i{
+         display: inline-block;
+         font-style: normal;
+     }
 </style>
